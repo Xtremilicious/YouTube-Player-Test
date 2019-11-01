e0c71694015094303af7a2ce17d7bfbf284f23d0
Xtremilicious <nilarjundas@outlook.com>
2019-11-01 14:09:42 +0530

    Updates

---
+++ precache-manifest.aa53d3b7e1033c9ef48fbb5acd372015.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "324a8759237123213e81b159c04ee962",
+    "url": "/YouTube-Player-Test/index.html"
+  },
+  {
+    "revision": "f91c0b821a895e2ef944",
+    "url": "/YouTube-Player-Test/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "a5e50fc0a7ea3db20ff2",
+    "url": "/YouTube-Player-Test/static/css/main.6aef0e12.chunk.css"
+  },
+  {
+    "revision": "f91c0b821a895e2ef944",
+    "url": "/YouTube-Player-Test/static/js/2.70c76fd2.chunk.js"
+  },
+  {
+    "revision": "a5e50fc0a7ea3db20ff2",
+    "url": "/YouTube-Player-Test/static/js/main.7bb067b4.chunk.js"
+  },
+  {
+    "revision": "bb54ade5561977fa7cb4",
+    "url": "/YouTube-Player-Test/static/js/runtime-main.4dc2c8ed.js"
+  }
+]);
